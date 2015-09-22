4cff5ce3206944dc5a28ee58e84c23aa4bd4b2e7
iurebrandao <iurebrandao@aluno.unb.br>
2015-09-22 10:06:06 -0300

    Poo
    Atualização do prof

---
+++ GameOfLife-Strategy/src/br/unb/cic/lp/gol/Main.java
@@ -0,0 +1,33 @@
+package br.unb.cic.lp.gol;
+
+import br.unb.cic.lp.gol.estrategias.Conway;
+
+/**
+ * Classe que define o metodo principal do programa; ou 
+ * seja, o metodo que vai ser executado pela JVM para inicializar 
+ * o programa. 
+ * 
+ * @author rbonifacio
+ */
+public class Main {
+
+	public static void main(String args[]) {
+		GameController controller = new GameController();
+		
+		Statistics statistics = new Statistics();
+
+		GameEngine engine = new GameEngine(10, 10, statistics);	
+		
+		//nessa implementacao, a estrategia do Conway eh 
+		//configurada como a estrategia inicial. 
+		engine.setEstrategia(new Conway());
+		
+		GameView board = new GameView(controller, engine);
+		
+		controller.setBoard(board);
+		controller.setEngine(engine);
+		controller.setStatistics(statistics);
+		
+		controller.start();
+	}
+}
